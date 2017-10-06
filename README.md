b555cbad4aca121d4506702b78d27ab9e014ffe1
Daniel Agbay <danielagbay@gmail.com>
2017-10-06 17:18:54 +0000

    updated index to show list of projects/hws

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 CSE 477, Web Application Architecture and Development, at MSU
 
 Each site is avaiable at: danielagbay.com/cse477/[SITE_DIRECTORY]
+or a list of all sites is avaiable at danielagbay.com/cse477/index.php
--- index.php
+++ index.php
@@ -1,4 +1,50 @@
 <?php
-require "/user/cse477/classweb/student/student.inc.php";
-$view = new Student\PortalView(__DIR__);
-echo $view->present();
+
+/* index for site on msu cse server */
+// require "/user/cse477/classweb/student/student.inc.php";
+// $view = new Student\PortalView(__DIR__);
+// echo $view->present();
+
+/* index for site on personal web server */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+
+<body>
+  <h2>List of CSE 477 projects/homework assignments</h2>
+
+  <ul>
+    <li><a href='project0'>Project 0</a></li>
+
+    <li><a href='project1'>Project 1</a></li>
+
+    <li><a href='project2'>Project 2</a></li>
+
+    <li><a href='project3'>Project 3</a></li>
+
+    <li><a href='step1'>Step 1</a></li>
+
+    <li><a href='step2'>Step 2</a></li>
+
+    <li><a href='step3'>Step 3</a></li>
+
+    <li><a href='step4'>Step 4</a></li>
+
+    <li><a href='step5'>Step 5</a></li>
+
+    <li><a href='step7'>Step 7</a></li>
+
+    <li><a href='step8'>Step 8</a></li>
+
+    <li><a href='step9'>Step 9</a></li>
+
+    <li><a href='step10'>Step 10</a></li>
+
+    <li><a href='ajaxnoir'>Step 11 (ajaxnoir)</a></li>
+  </ul>
+
+</body>
+</html>
